39ada8044b5308fbfba1345a12e9a18d0da85528
Dlabins <DLABINS1895@GMAIL.COM>
2020-12-19 11:04:58 -0500

    created database and tables

---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+-- Created department table
+CREATE TABLE department (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(30)
+ 
+);
+-- Created role table
+CREATE TABLE role (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
+);
+-- Created employee table
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  manager_id INT,
+  role_id INT
+);
+
+-- Selects all from the 3 tables made above
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
